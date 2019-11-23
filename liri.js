75192164e9e6a03635e03f42b03fae2111050939
Nadia-Kadri <nadiagrace96@gmail.com>
2019-11-23 18:59:50 -0500

    omdb api

--- liri.js
+++ liri.js
@@ -4,32 +4,70 @@
 
 // var spotify = new Spotify(keys.spotify);
 
+const axios = require("axios");
 
-// var command = process.argv[2];
+const fs = require("fs");
 
-// switch (command) {
-//     case "concert-this":
-//       break;
-    
-//     case "spotify-this-song":
-//       break;
-    
-//     case "movie-this":
-//       break;
-    
-//     case "do-what-it-says":
-//       break;
-//     }
+let command = process.argv[2];
+
+let input = process.argv.slice(3).join(" ");
+
+switch (command) {
+  case "concert-this":
+      searchArtist(input);
+      break;
+  case "spotify-this-song":
+      searchSong(input);
+      break;
+  case "movie-this":
+      searchMovie(input);
+      break;
+  default:
+      console.log("Please type concert-this followed by your query");
+      break;
+}
+
+function searchArtist(artist) {
+  let URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+  axios.get(URL).then(function (response) {
+      console.log(response.data)
+  }).catch(function(error) {
+    if (error.response) {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      console.log(error.request);
+    } else {
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  });
+}
 
 
-// Basic Node application for requesting data from the OMDB website via axios
-// Here we incorporate the "axios" npm package
-var axios = require("axios");
-
-// We then run the request with axios module on a URL with a JSON
-axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
-  function(response) {
-    // Then we print out the imdbRating
-    console.log("The movie's rating is: " + response.data.imdbRating);
+function searchMovie(movie) {
+  if (!movie) {
+    movie = "Mr. Nobody";
   }
-);
+
+  let URL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+  axios.get(URL).then(function(response) {
+
+    const data = response.data;
+
+    console.log(`Title: ${data.Title}\nRelease Year: ${data.Year}\nIMDB Movie Rating: ${data.imdbRating}\nRotten Tomatoes Movie Rating: ${data.Ratings[1].Value}\nCountry: ${data.Country}\nLanguage: ${data.Language}\nPlot: ${data.Plot}\nActors: ${data.Actors}`);
+  })
+  .catch(function(error) {
+    if (error.response) {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      console.log(error.request);
+    } else {
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  });
+}
